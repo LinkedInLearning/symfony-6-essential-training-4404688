e5d574ecc4584c6ef64af627a6b4a6b80c542ceb
Gary Kovar <gary@craftpeak.com>
2023-04-10 16:17:04 -0400

    build stuff

--- my_project/config/bundles.php
+++ my_project/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
